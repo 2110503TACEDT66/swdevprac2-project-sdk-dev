5908ab91182b71eb2ac35f31823ad6e00c54f1aa
73215103 <73215103+SIER101@users.noreply.github.com>
2024-03-25 10:02:01 +0700

    add persist

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,33 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import bookSlice from "./features/bookSlice";
 import { TypedUseSelectorHook, useSelector } from "react-redux";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "rootPersist",
+  storage,
+};
+
+const rootReducer = combineReducers({ bookSlice });
+const reduxPersistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    bookSlice,
-  },
+  reducer: reduxPersistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
