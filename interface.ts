7eff669b0f5335c42e7e4cb9682cf596ae7b8da3
73215103 <73215103+SIER101@users.noreply.github.com>
2024-03-26 22:40:43 +0700

    remove hospital

--- interface.ts
+++ interface.ts
@@ -1,23 +1,3 @@
-interface HospitalItem {
-  _id: string;
-  name: string;
-  address: string;
-  district: string;
-  province: string;
-  postalcode: string;
-  tel: string;
-  picture: string;
-  __v: number;
-  id: string;
-}
-
-interface HospitalJson {
-  success: boolean;
-  count: number;
-  pagination: Object;
-  data: HospitalItem[];
-}
-
 export class HotelItem {
   _id: string = "";
   name: string = "";
